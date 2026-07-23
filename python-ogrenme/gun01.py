8180c327a8ae14827ccf30614f66a4570b855437
Gülser Güralp <gulserguralp05@gmail.com>
2026-07-23 20:53:31 +0300

    Gün 1: değişkenler ve tipler

---
+++ python-ogrenme/gun01.py
@@ -0,0 +1,20 @@
+ad = "Gülser"
+print(ad)
+
+yas =21
+boy = 1.61
+ogrenci = True
+
+print(f"merhaba {ad}, yaşın {yas}")
+
+print(type(ad))
+print(type(yas))
+print(type(boy))
+print(type(ogrenci))
+
+sayi_metin ="42"
+sayi = int(sayi_metin)
+
+print(sayi + 8)
+print(int(sayi_metin)+8)
+print(sayi_metin + str(8))
